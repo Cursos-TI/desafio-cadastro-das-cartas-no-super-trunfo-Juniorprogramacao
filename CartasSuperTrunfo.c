24fe0f63f7af2365fadd8f5a02d3e146fd14ebd9
Juniorprogramacao <janildojunior91@gmail.com>
2025-03-12 23:45:49 +0000

    Criado variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,20 @@
 //Teste larissa
 
 int main() {
+ //Adicionar Variáveis
+ 
+    char estado1, estado2 [50]; //c1 representa carta 1 c2 representa a carta 2
+    char codigo1, codigo2 [50];
+    char cidade1, cidade2 [60];
+    
+    int populacao1, populacao2;
+    int pontosturisticos1, pontosturisticos2;
+    float area1, area2;
+    float pib1, pib2;
+  
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -17,6 +31,8 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+   
 
     return 0;
 }
