3b29f8fdfca5f00e464711c6a926de62265b304c
Juniorprogramacao <janildojunior91@gmail.com>
2025-03-13 18:57:49 +0000

    Adicionado Variaveis

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,8 @@
 //Teste larissa
 
 int main() {
- //Adicionar Variáveis
+ 
+    //Adicionar Variáveis
  
     char estado1[50], estado2[50]; //c1 representa carta 1 c2 representa a carta 2
     char codigo1[50], codigo2[50];
@@ -18,7 +19,9 @@ int main() {
     float area1[20], area2[20];
     float pib1[20], pib2[20];
 
-    printf ("Digite o nome do Estado\n");
+// carta 1
+
+    printf ("Digite o Estado que é uma letra de 'A' a 'H'\n");
     scanf  ("%s", &estado1);
 
     printf ("Digite o código da cidade\n");
@@ -33,6 +36,15 @@ int main() {
     printf ("Digite o número de pontos turísticos\n");
     scanf  ("%d", &pontosturisticos1);
 
+    printf ("Digite a área\n");
+    scanf  ("%f", &area1);
+
+    Printf ("Qual é o PIB?\n");
+    scanf  ("%f", &pib1);
+
+    
+
+    
     
   
 
